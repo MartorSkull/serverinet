d9bed0b732136321654f4a4e14b00c61640366ea
MartorSkull <martin_montane_6@hotmail.com>
2018-10-09 17:33:50 -0300

    this is done

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inet.urls')),
     path('login', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
